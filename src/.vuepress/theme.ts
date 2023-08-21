f187d0dd73a43ac27ed5bacf8282c9a4659464b6
Dennis <374627149@qq.com>
2023-08-21 17:27:33 +0800

    更换评论服务

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -42,7 +42,7 @@ export default hopeTheme({
     comment: {
       // You should generate and use your own comment service
       provider: 'Waline',
-      serverURL: 'https://waline-comment.vuejs.press',
+      serverURL: 'https://waline.dennisdong.top',
     },
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
